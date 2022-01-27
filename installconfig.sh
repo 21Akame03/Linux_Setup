75de89465c820f40340dc4bc96a43f02761501d6
21Akame03 <21akame03@gmail.com>
2022-01-27 16:01:36 +0400

    -m

--- installconfig.sh
+++ installconfig.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 
 # export current directory to path
-echo "export PATH='$PWD:$PATH'"
+echo "export PATH='$PWD:$PATH'" > ~/.bashrc
 echo "dbox() { sh dbox.sh $1 } " > ~/.bashrc
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # install starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
@@ -16,15 +19,6 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 
 mkdir ~/.config/nvim  
 cp ./init.vim ~/.config/nvim/init.vim
-
-# install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# install go
-wget https://go.dev/dl/go1.17.6.linux-amd64.tar.gz ~/Documents
-tar xvzf ~/Documents/go1.17.6.linux-amd64.tar.gz
-mv go1.17.6.linux-amd64.tar.gz/ ~/
-echo "export PATH='~/home/akame/go/bin:$PATH'" > ~/.bashrc
 
 # install python 
 sudo dnf install python python-pip -y
